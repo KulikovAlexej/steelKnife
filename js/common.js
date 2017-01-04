c1d2cf337c672849be356b847ae080e5acc75416
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2017-01-05 01:46:43 +0300

    Есть полностью адаптивные хедер и сайдбар. На гамбургере пока нет обработчика.
    Хочу сделать интересное съезжание сайдбара с одновременным уменьшением/увеличением ширины блока контента (это для разрешения экрана от 993 до 1200px). На меньших экранах - проще.

--- js/common.js
+++ js/common.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
   		backgroundColor: 'red',
   	})
   })
-
+  change_sidebar();
+  $(window).resize(function(){
+  	change_sidebar();
+  })
+function change_sidebar(){
+	$window_width = $(window).width();
+  	$window_height = $(window).height();
+  	$aside_height = $('aside').outerHeight();
+  	if($window_width > 1200 && $aside_height > $window_height){
+  		$('aside').css({
+			position: 'absolute',
+			width: '100%'
+		})
+  	}
+	
+}
   
 });
